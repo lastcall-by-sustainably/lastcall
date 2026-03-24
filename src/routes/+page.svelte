4b50d19e8071b4d5c245875d018a97eebcf56165
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-03-24 19:52:13 -0400

    Fix CLS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,14 @@
 </script>
 
 <div class="flex flex-col items-center gap-16 p-8">
-  <enhanced:img class="size-full" src="$lib/assets/logo-with-tagline.png" alt="LastCall logo with tagline" />
+  <div class="size-full">
+    <enhanced:img
+      src="$lib/assets/logo-with-tagline.png"
+      width="708"
+      height="978"
+      alt="LastCall logo with tagline"
+    />
+  </div>
 
   <div class="w-full max-w-72 flex flex-col gap-4">
     <Button class="h-16 w-full text-xl font-semibold rounded-full border-border border-4" variant="default" size="lg" href="/browse">Log in</Button>
