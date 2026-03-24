99de6b00407f121502537b81d3001bfde51350f6
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-03-24 19:56:33 -0400

    Add loading bar for slow connections

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './layout.css';
-	import { page } from '$app/state';
+	import { page, navigating } from '$app/state';
+  import { fade } from 'svelte/transition';
 	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner';
@@ -15,6 +16,13 @@
 <ModeWatcher defaultMode="light" />
 
 <Toaster richColors />
+
+{#if navigating.to}
+  <div
+		class="fixed top-0 left-0 z-50 h-1 w-full animate-slide-gradient bg-linear-to-r from-primary via-[#b8d9b4] to-primary bg-size-[200%_100%]"
+    in:fade={{ delay: 300 }}
+  ></div>
+{/if}
 
 <div class={['min-h-svh flex flex-col items-center max-w-md mx-auto', page.url.pathname === '/' && 'bg-[#e6f5de]']}>
 	<Header />
