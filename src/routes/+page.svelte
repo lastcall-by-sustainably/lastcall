acfd244d9dc84331fcdbdf382b65c9234d8b92ee
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-03-24 14:08:13 -0400

    Add homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="flex flex-col items-center gap-16 p-8">
+  <enhanced:img class="size-full" src="$lib/assets/logo-with-tagline.png" alt="LastCall logo with tagline" />
+
+  <div class="w-full max-w-72 flex flex-col gap-4">
+    <Button class="h-16 w-full text-xl font-semibold rounded-full border-border border-4" variant="default" size="lg" href="/browse">Log in</Button>
+    <Button class="h-16 w-full text-xl font-semibold rounded-full border-border border-4" variant="outline" size="lg" href="/browse">Sign up</Button>
+  </div>
+</div>
